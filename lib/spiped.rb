95dc8020ce76d1be15320fc419e49c0f3f47d50f
Brian P O'Rourke <bpo@somnambulance.net>
2014-09-08 17:46:38 -0700

    Nuke the socket path, for unix sockets.

--- lib/spiped.rb
+++ lib/spiped.rb
@@ -9,14 +9,18 @@ class Spiped
   SPIPED = File.join(BASE, "spiped", "spiped" )
 
   #
-  # 1. Write the provided base64-encoded key to disk
-  # 2. Spawn spiped to send plaintext received on `source` encrypted to spiped
+  # 1. Delete the 'source' socket file if it exists.
+  # 2. Write the provided base64-encoded key to disk.
+  # 3. Spawn spiped to send plaintext received on `source` encrypted to spiped
   #    running at `target`
   #
   def self.spawn_encryptor( source, target, key, verbose = false )
+    File.delete(source) if File.exist?(source)
+
     keyfile = Tempfile.new("spiped")
     keyfile.write Base64.decode64(key)
     keyfile.close
+
     cmd = [SPIPED, '-e', '-s', source, '-t', target, '-k', keyfile.path, '-D']
     if verbose
       $stdout.puts "Running #{cmd.join(' ')}"
